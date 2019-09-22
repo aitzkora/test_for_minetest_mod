cd2821ba360f598470eacd1ecfe6fce241b9b3c9
aitzkora <marc.fuentes@aquilenet.fr>
2019-09-22 10:20:56 +0200

    add my very first  mod : the bouncing ball

---
+++ init.lua
@@ -0,0 +1,6 @@
+minetest.register_node("bouncing_ball:ball", {
+        description = "the bouncing ball",
+        tiles = {"bouncing.png"},
+        is_ground_content = true,
+        groups = {cracky=3, stone=1}
+})
