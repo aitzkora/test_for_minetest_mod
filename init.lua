76bd75cbf86674e6f94a3e0d7bac05b4537f5d99
aitzkora <marc.fuentes@aquilenet.fr>
2019-09-22 12:25:31 +0200

    Make a moving cube toward upstair when it is punched

--- init.lua
+++ init.lua
@@ -2,5 +2,10 @@ minetest.register_node("bouncing_ball:ball", {
         description = "the bouncing ball",
         tiles = {"bouncing.png"},
         is_ground_content = true,
-        groups = {cracky=3, stone=1}
+        groups = {cracky=3, stone=1},
+        on_punch = function (pos, node, puncher)
+              minetest.chat_send_all('pos = {'..pos.x..', '..pos.y..', '..pos.z..' }') 
+              minetest.set_node({ x = pos.x, y = pos.y + 1, z = pos.z }, {name ="bouncing_ball:ball"})
+              minetest.set_node(pos, { name = "air" })
+        end 
 })
