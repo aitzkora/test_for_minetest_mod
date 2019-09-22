11737ed6e3f24f19b8ce9a1d9b7f88d1aa47b235
aitzkora <marc.fuentes@aquilenet.fr>
2019-09-22 14:06:07 +0200

    try to move a sphere : not yet working

--- init.lua
+++ init.lua
@@ -1,11 +1,39 @@
-minetest.register_node("bouncing_ball:ball", {
+local timer = 0
+minetest.register_tool("bouncing_ball:ball", {
         description = "the bouncing ball",
-        tiles = {"bouncing.png"},
-        is_ground_content = true,
-        groups = {cracky=3, stone=1},
-        on_punch = function (pos, node, puncher)
-              minetest.chat_send_all('pos = {'..pos.x..', '..pos.y..', '..pos.z..' }') 
-              minetest.set_node({ x = pos.x, y = pos.y + 1, z = pos.z }, {name ="bouncing_ball:ball"})
-              minetest.set_node(pos, { name = "air" })
+        inventory_image = "bouncing.png",
+        on_use = function (itemstack, user, pointed_thing)
+             local pos = user:getpos()
+             local dir = user:get_look_dir()
+             if pos and dir then
+                 pos.y = pos.y + 1.6
+                 local obj = minetest.add_entity(pos, "bouncing_ball:my_bouncing_ball")
+                 if obj then
+                    obj.direction = {x=dir.x, y=dir.y , z=dir.z }
+                 end
+             end
         end 
 })
+
+local my_bouncing_ball = {
+    initial_properties = {
+        physical = false,
+        timer = 0,
+        textures = {"sphere.png"},
+        direction = {x=0., y=0.,z=0.},
+    },
+
+    message = "This is entity to represent a bouncing ball",
+}
+
+my_bouncing_ball.on_step = function(self, dtime, node, pos)
+    --self.timer = self.timer + dtime
+    local pos = self.object:getpos()
+    local vel = self.object.direction
+    -- currently x  += v  * dt (rectiline movement)
+    local pos_next = vector.add(pos, {x=vel.x * dtime, y=vel.y * dtime, z= vel.z *dtime})
+    if minetest.get_node(pos_next).name == "air" then
+        self.object:move_to(pos_next)
+    end
+end
+minetest.register_entity("bouncing_ball:my_bouncing_ball", my_bouncing_ball)
